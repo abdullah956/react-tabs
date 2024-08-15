8fbca95b547549cd88def3b9803519a1c3e96a46
abdullah956 <abdullaharshed956@gmail.com>
2024-08-15 08:41:36 +0500

    tabs-done

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import TabTest from "./components/custom-tabs/tab-test";
+function App() {
+  return (
+    <div className="App">
+      <TabTest/>
+    </div>
+  );
+}
+
+export default App;
